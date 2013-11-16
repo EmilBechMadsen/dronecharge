fb0a71b3bb83a0a1fbbee9222256c215de8dd812
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2013-11-16 11:41:32 +0100

    add landing task

---
+++ dckit/tasks/landing_task.py
@@ -0,0 +1,29 @@
+from dckit.tasks.task import Task, TaskState
+
+
+class LandingTask(Task):
+    def __init__(self, name, targetPosition):
+        super(LandingTask, self).__init__(name)
+
+        self.isCompleted = False
+        self.targetPosition = targetPosition
+        self.required_capabilities = [
+            "move"
+        ]
+
+    def start(self):
+        self.state = TaskState.EXECUTING
+        self.drone.move(self.targetPosition)
+
+    def isComplete(self):
+        if self.drone.actualPosition == self.drone.targetPosition:
+            self.isCompleted = True
+        return self.isCompleted
+
+    def __repr__(self):
+        ret = "\n\t<LandingTask (" + self.name + ") "
+        ret += " State: " + str(self.state)
+        if len(self.subtasks):
+            ret += "\n\t" + str(self.subtasks) + "\n\t"
+
+        return ret
